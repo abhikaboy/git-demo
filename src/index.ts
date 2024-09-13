8c4b747076138e93c6aa6798a72e422ed9087e76
michaelp2002 <85465378+michaelp2002@users.noreply.github.com>
2024-09-13 15:01:01 -0400

    print hello message

--- src/index.ts
+++ src/index.ts
@@ -6,3 +6,5 @@ const result_add = calc.add(2, 3);
 console.log(`result_add = ${result_add}`);
 const result_sub = calc.subtract(5, 3);
 console.log(`result_add = ${result_sub}`);
+const result_hello = calc.print_hello();
+console.log(`result_hello = ${result_hello}`);
