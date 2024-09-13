4cfedf8bc85c91ccce9617e5e26b93a6248984c6
nicoleli26 <nnicoleli22@gmail.com>
2024-09-13 14:55:08 -0400

    implement divide operation

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,3 @@ const calc = new Calculator();
 
 const result_add = calc.add(2, 3);
 console.log(`result_add = ${result_add}`);
-const result_sub = calc.subtract(5, 3);
-console.log(`result_add = ${result_sub}`);
