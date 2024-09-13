a3a6674be10b5b42bf7a3a5b8e46e19470b028d2
Abhik Ray <abhikaboy@gmail.com>
2024-09-13 14:41:14 -0400

    add subsctract operation

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,5 @@ const calc = new Calculator();
 
 const result_add = calc.add(2, 3);
 console.log(`result_add = ${result_add}`);
-
+const result_sub = calc.subtract(5, 3);
+console.log(`result_add = ${result_sub}`);
